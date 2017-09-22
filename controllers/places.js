f437ec6abe33e5e5058df647dbbbb709bf937928
Wayne Williamson <hedgehog1982@gmail.com>
2017-09-22 10:56:16 +0100

    added pug file to render the results

--- controllers/places.js
+++ controllers/places.js
@@ -1,32 +1,27 @@
-function mapAndPics(results){  
-      //console.log("first results " ,results[0]);
-      /// remove parts of results i dont want
-      var cleanArray = results.map(function (x){
-          var newArray = [];
-          //console.log("name is ", x.name , " photos is", typeof x.photos[0].photo_reference);
-          newArray.name = x.name;
-          if (typeof x.photos != "undefined"){ // see if it exists before I try and access it
-              newArray.photoRef = x.photos[0].photo_reference;  // use this photoReference to get phot
-                getPics(newArray.photoRef, function(picture){
-                  newArray.photoLink = picture;
-                });
-            } else {
-              newArray.photoRef ="No Photo";
-            }
-          newArray.address = x.formatted_address;
-            return newArray
-      })
+function mapAndPics(results, callback) {
+  console.log(results[0])
+  var cleanArray = results.map(function(x) {  ///resulting in not being able to send it back ? build html this side?
+    var newArray = [];
+    //console.log(results[0]);
+    newArray.name = x.name;
+    if (typeof x.photos != "undefined") { // see if it exists before I try and access it
+      newArray.photoRef = process.env.GpicQuery + x.photos[0].photo_reference; // use this photoReference to get photo was going to cache it but dont need to
+    } else {
+      newArray.photoRef = "No Photo";
+    }
+    newArray.address = x.formatted_address;
 
-      console.log(cleanArray);
+    return {"name" : newArray.name, "photoRef" : newArray.photoRef, "address" : newArray.address};
+  });
+
+
+
+
+  callback (null, cleanArray);
 }
-
-function getPics (photoRef, callback){
-    callback("file.png")
-}
-
 
 // Get home page
-var https = require('https'); //for get request to wokr
+var https = require('https'); //for get request to work
 
 exports.getHome = function(req, res) {
   res.render('index', {
@@ -34,29 +29,37 @@ exports.getHome = function(req, res) {
   });
 };
 
-// Get
+// Get places
 exports.searchPlacePost = function(req, res) {
   console.log("recieved search", req.body.search);
   const mapUrl = process.env.GURL + process.env.GApiKey + process.env.Gquery + req.body.search;
-  console.log("map url is ", mapUrl)
+  console.log("map url is ", mapUrl);
 
-  https.get(mapUrl, function(res) {
+
+  https.get(mapUrl, function(result) {
     console.log("in get");
-    res.setEncoding("utf8");;
+    result.setEncoding("utf8");;
     var body = "";
 
-    res.on("data", function(chunk) { //while recieving data add to body
+    result.on("data", function(chunk) { //while recieving data add to body
       body += chunk;
     });
 
-    res.on("end", function() {  //on end of data
-      body = JSON.parse(body);  //in json format so we can manipulate data
-      //console.log(body.results)
-      mapAndPics(body.results);
-      // need to map this to something nicer to manipulate and store on DB (....the DB i havent even set up)
+    result.on("end", function() { //on end of data
+      body = JSON.parse(body); //in json format so we can manipulate data
+      mapAndPics(body.results, function (err, data){ //maps to a new array and passes back
+      //console.log("callback was ", data);
+      //  console.log(data[0])
+        //res.send(JSON.stringify(data));
+        //res.send(data[1]);
+        res.render('canidothis',{
+          placesList : data
+        });
+      })
+
     })
 
-    req.on('error', function(error) { //if an error
+    result.on('error', function(error) { //if an error
       console.log('ERROR: ' + error.message);
     });
 
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,10 @@
 $(document).ready(function() {
-
-
-
-
-
     $("#submit").click(function(event) {
         event.preventDefault();
         var search = $("#search").val();
         console.log(submit)
         $.post("/search", { "search" : search }, function(data) {
+          $("#posthtml").append(data);
               console.log(data);
           });
         });
